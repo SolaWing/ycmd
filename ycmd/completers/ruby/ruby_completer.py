294624924d38615a39d95ca2764e221b0fd38886
solawing <316786359@qq.com>
2021-09-07 14:36:29 +0800

    use pid instead ppid as solargraph port id

--- ycmd/completers/ruby/ruby_completer.py
+++ ycmd/completers/ruby/ruby_completer.py
@@ -196,7 +196,7 @@ class RubyCompleter( language_server_completer.LanguageServerCompleter ):
           if useSocket:
             # 简化实现，真实的需要listen一下才知道
             self._connection_type = "tcp"
-            self._port = os.getppid() % 55536 + 10000
+            self._port = os.getpid() % 55536 + 10000
             if self._use_bundler:
                 self._command_line = ['bundle', 'exec', lang_server_bin, "socket", "-p", str(self._port)]
             else:
